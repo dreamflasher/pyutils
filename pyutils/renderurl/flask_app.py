f7a8d6301d43d72029e479192e525d69e851b3cc
Marcel <31695+dreamflasher@users.noreply.github.com>
2023-09-09 20:43:49 +0200

    .

--- pyutils/renderurl/flask_app.py
+++ pyutils/renderurl/flask_app.py
@@ -3,6 +3,7 @@ from typing import List
 
 from flask import Flask, render_template
 from pyutils.renderurl.secret import app_pass
+from selenium.webdriver.chrome.options import Options
 from flask import request
 app = Flask(
     __name__,
@@ -24,13 +25,19 @@ def fetch() -> str:
         abort(404)
         return ""
 
-    with Browser("chrome", headless=True, user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0") as browser:
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+
+    with Browser("chrome", headless=True, chrome_options=chrome_options, user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0") as browser:
         browser.visit(request.args["url"])
         time.sleep(10)
 
         return browser.html
 
-    
+
 
 
 if __name__ == "__main__":
